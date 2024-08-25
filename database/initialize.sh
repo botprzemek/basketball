d7eb3d07f0178d1dbf56d8a2f962d63ab749cb01
botprzemek <info@botprzemek.pl>
2024-08-25 23:42:43 +0200

    fix(database): fixed newline error in cockroach shell script

--- database/initialize.sh
+++ database/initialize.sh
@@ -1,18 +1,21 @@
-./cockroach start \
-  --insecure \
-  --cache=25% \
-  --max-sql-memory=25% \
+./cockroach start                               \
+  --insecure                                    \
+  --cache=25%                                   \
+  --max-sql-memory=25%                          \
   --locality=region=eu-central,zone=eu-poland-1 \
-  --join=database-1:26357,database-2:26357,database-3:26357 \
-  --listen-addr=database-1:26357 \
-  --sql-addr=database-1:26257 \
+  --listen-addr="database-1:26357"              \
+  --sql-addr="database-1:26257"                 \
+  --join=database-1:26357,database-2:26357,database-3:26357
+
 ./cockroach init \
-  --insecure \
+  --insecure     \
   --host="basketball-database-1:26357"
-./cockroach sql
-  --insecure \
+
+./cockroach sql                        \
+  --insecure                           \
   --host="basketball-database-1:26257" \
   --execute="CREATE DATABASE basketball; CREATE USER basketball WITH PASSWORD NULL; ALTER DATABASE basketball OWNER TO basketball; CREATE TYPE basketball.position_enum AS ENUM ('PG', 'SG', 'SF', 'PF', 'C');"
-./cockroach node status
-  --insecure \
+
+./cockroach node status \
+  --insecure            \
   --host="basketball-database-1:26257"
